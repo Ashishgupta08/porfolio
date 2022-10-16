921495cc88071fb15b2b84daec4d6a7f5b61d27d
Ashish Gupta <ashish08rewa@gmail.com>
2022-10-16 10:54:24 +0530

    tailwind css added

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Ashish Gupta</h1>
+      <h1 className="text-3xl font-bold">
+        Ashish Gupta
+      </h1>
     </div>
   )
 }
